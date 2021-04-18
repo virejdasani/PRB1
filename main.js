2fb86ea06e7cfbfb4f8e6784af07ce815b1179ba
Virej Dasani <dasanivirej@gmail.com>
2021-04-18 19:52:11 +0530

    Can now check if in focus and if not then hide itself

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow() {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: 350,
-        height: 550,
+        height: 450,
         resizable: false,
         frame: false,
         show: false
@@ -30,14 +30,35 @@ function createWindow() {
 
     // tray onClick event
     tray.on('click', () => {
+        // If the window is visible and the tray icon is clicked, it should become hidden and vice versa
         if (mainWindow.isVisible()) {
             mainWindow.hide()
         } else {
             mainWindow.show()
         }
+
+
     })
 
+    // This is to check is the window is in focus
+    function checkIfFocused() {
+        // If the window is in focus
+        if (mainWindow.isFocused()) {
+            // console.log('Focused')
+            // mainWindow.hide()
+        }
+        // If the window is not in focus
+        else {
+            mainWindow.hide()
+        }
+        // Check if window is in focus every 5 seconds
+        setTimeout(checkIfFocused, 5000)
+    }
+    // Initial function call
+    checkIfFocused()
+
 }
+
 
 app.whenReady().then(() => {
     createWindow()
